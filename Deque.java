f14260c241e60dd1221fb0f12cea0f813c4633e4
doniatung <doniatung32@gmail.com>
2017-03-31 08:25:33 -0400

    Create Deque.java

---
+++ Deque.java
@@ -0,0 +1,17 @@
+public Interface Deque<T>{
+
+
+  public void addLast(T x);
+  
+  public void removeFirst();
+  
+  public int size();
+  
+  public T peekFirst();
+  
+  public T peekLast();
+  
+}
+  
+  
+  
