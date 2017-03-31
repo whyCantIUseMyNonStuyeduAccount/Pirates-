dd43173404dd80403000d63e63f6fa556a25e94f
dimahvirstuy <dhvirtsman@stuy.edu>
2017-03-31 08:34:00 -0400

    Did the thing

---
+++ LLDeque.java
@@ -0,0 +1,36 @@
+public class LLDeque implements Deque{
+    
+    new LLNode<T> first, last;
+    int size;
+    
+    public LLDeque() {
+	first = null;
+	last = null;
+	size = 0;
+    }
+    
+    public void addLast (T node) {
+	last.setNext(node);
+	last = node;
+    }
+    public void removeFirst() {
+	first = first.getNext();
+    }
+    public int size() {
+	return size;
+    }
+    public T peekFirst() {
+	return first._cargo;
+    }
+    public T peekLast() {
+	return last._cargo;
+    }
+    public boolean isEmpty() {
+	return size == 0;
+    }
+    public static void main(String[] args) {
+
+    }
+
+    
+	
