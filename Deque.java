df09c20e1dafde56f89f68c16dc546c49c6db4b0
clei1 <clei1@stuy.edu>
2017-04-02 20:37:29 -0400

    Update Deque.java

--- Deque.java
+++ Deque.java
@@ -1,9 +1,12 @@
 public interface Deque<T>{
 
-
+  public void addFirst(T x);
+  
   public void addLast(T x);
   
   public void removeFirst();
+  
+  public void removeLast();
   
   public int size();
   
