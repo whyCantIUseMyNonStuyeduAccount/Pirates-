2011560c7f4f872d8b8f0ebd70d89a37b22812de
whyCantIUseMyNonStuyeduAccount <nborisov@stuy.edu>
2017-04-05 08:26:29 -0400

    Added comments to interface

--- Deque.java
+++ Deque.java
@@ -1,17 +1,24 @@
 public interface Deque<T>{
 
+    // Adds a node containing x as cargo to beginning of Deque
     public void addFirst(T x);
   
+    // Adds a node containing x as cargo to end of Deque
     public void addLast(T x);
-  
+
+    // Removes a node from the beginning of Deque and returns its cargo
     public T removeFirst();
 
+    // Removes a node from the end of Deque and returns its cargo
     public T removeLast();
     
+    // Returns the size of the Deque
     public int size();
   
+    // Returns cargo of the first node
     public T peekFirst();
   
+    // Returns cargo of the last node
     public T peekLast();
   
 }
